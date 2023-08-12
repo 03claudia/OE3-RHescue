83573c706b3aea60572f65ec15f273f546ad0419
rodrigo0345 <rodrigocralha@gmail.com>
2023-08-12 16:11:03 +0100

    Adicionei alguns exceis da drive aqui ao repositório e coloquei o codigo antigo dentro da pasta adequada

---
+++ app.py
@@ -0,0 +1,43 @@
+
+
+# Entry point do programa
+from layout import Layout
+
+layout_example = """
+{
+  "members": [
+    {
+      "name": "Molecule Man",
+      "age": 29,
+      "secretIdentity": "Dan Jukes",
+      "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+    },
+    {
+      "name": "Madame Uppercut",
+      "age": 39,
+      "secretIdentity": "Jane Wilson",
+      "powers": [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
+    },
+    {
+      "name": "Eternal Flame",
+      "age": 1000000,
+      "secretIdentity": "Unknown",
+      "powers": [
+        "Immortality",
+        "Heat Immunity",
+        "Inferno",
+        "Teleportation",
+        "Interdimensional travel"
+      ]
+    }
+  ]
+}
+"""
+
+if __name__ == "__main__":
+    layout = Layout(layout_example, "layout.json")
+    print(layout.get_data())
