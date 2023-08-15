daed433c7a7a51e9c819cf93ea6cb904766a8cc1
rodrigo0345 <rodrigocralha@gmail.com>
2023-08-15 03:35:24 +0100

    o excel de output já está melhorzito

--- src/Printers/ExcelPrinter.py
+++ src/Printers/ExcelPrinter.py
@@ -72,42 +72,11 @@ class ExcelPrinter:
         self.layout = layout
 
     def print(self, filepath):
-        data: dict[str: str] = self.__process_output_layout(self.layout)
+        data: dict = self.__process_output_layout(self.layout)
         self.__save_file(data, filepath)
     
     """
-    data = {
-        'Rafaela Carvalho': {
-            'Catarina Milheiro': '6', 
-            'Gonçalo Figueiredo': '5', 
-            'Inês Cabral': '5'
-        }, 
-        'Paula Ferreira': {
-            'Catarina Milheiro': '6', 
-            'Gonçalo Figueiredo': '4', 
-            'Inês Cabral': '4'
-        }, 
-        'Mariana Oliveira': {
-            'Catarina Milheiro': '6', 
-            'Gonçalo Figueiredo': '3', 
-            'Inês Cabral': '5'
-        }, 
-        'Inês Cabral': {
-            'Catarina Milheiro': '6', 
-            'Gonçalo Figueiredo': '6', 
-            'Inês Cabral': '6'
-        }, 
-        'Mariana Arezes': {
-            'Catarina Milheiro': '6', 
-            'Gonçalo Figueiredo': '5', 
-            'Inês Cabral': '6'
-        }, 
-        'Paulo Vieira': {
-            'Catarina Milheiro': '6', 
-            'Gonçalo Figueiredo': '5', 
-            'Inês Cabral': '5'
-        }, 
-        'Gonçalo Figueiredo': {'Catarina Milheiro': '6', 'Gonçalo Figueiredo': '4', 'Inês Cabral': '6'}, 'Catarina Milheiro': {'Catarina Milheiro': '6', 'Gonçalo Figueiredo': '4', 'Inês Cabral': '4'}, 'Inês Bastos': {'Catarina Milheiro': '6', 'Gonçalo Figueiredo': '4', 'Inês Cabral': '4'}}
+    content = {'Rafaela Carvalho': {'Catarina Milheiro': ['6', '6', '6'], 'Gonçalo Figueiredo': ['5', '5', '5'], 'Inês Cabral': ['5', '5', '5']}, 'Paula Ferreira': {'Catarina Milheiro': ['6', '6', '6'], 'Gonçalo Figueiredo': ['4', '5', '4'], 'Inês Cabral': ['4', '5', '5']}, 'Mariana Oliveira': {'Catarina Milheiro': ['6', '6', '6'], 'Gonçalo Figueiredo': ['3', '2', '2'], 'Inês Cabral': ['5', '5', '6']}, 'Inês Cabral': {'Catarina Milheiro': ['6', '6', '6'], 'Gonçalo Figueiredo': ['6', '4', '4'], 'Inês Cabral': ['6', '5', '6']}, 'Mariana Arezes': {'Catarina Milheiro': ['6', '5', '6'], 'Gonçalo Figueiredo': ['5', '5', '5'], 'Inês Cabral': ['6', '5', '5']}, 'Paulo Vieira': {'Catarina Milheiro': ['6', '6', '6'], 'Gonçalo Figueiredo': ['5', '4', '4'], 'Inês Cabral': ['5', '4', '4']}, 'Gonçalo Figueiredo': {'Catarina Milheiro': ['6', '6', '6'], 'Gonçalo Figueiredo': ['4', '4', '5'], 'Inês Cabral': ['6', '6', '6']}, 'Catarina Milheiro': {'Catarina Milheiro': ['6', '6', '6'], 'Gonçalo Figueiredo': ['4', '4', '4'], 'Inês Cabral': ['4', '4', '4']}, 'Inês Bastos': {'Catarina Milheiro': ['6', '6', '6'], 'Gonçalo Figueiredo': ['4', '3', '5'], 'Inês Cabral': ['4', '4', '5']}}
     """
     def __process_output_layout(self, input_data: Layout) -> dict[str: str]:
         hashmap = dict[str: str]()
@@ -153,11 +122,13 @@ class ExcelPrinter:
                     content.update({measurer_name: {}})
                 
                 if measured_name not in content[measurer_name].keys():
-                    content[measurer_name].update({measured_name: grade})
+                    content[measurer_name].update({measured_name: []})
+
+                content[measurer_name][measured_name].append(grade)
 
         hashmap[self.settings][Type.MEASURER.name]["row-span"] = len(hashmap.keys()) - 1
-        print(hashmap)
-        return []
+        print(hashmap[self.content])
+        return hashmap
     
     def __process_dimentions_of(self, type: Type, data: Layout) -> { "row-span": int, "col-span": int}:
         obj = data.get_type(type)
@@ -191,9 +162,23 @@ class ExcelPrinter:
         content = data[self.content]
 
         # Definir o header
+
         ws.append(headers)
 
-        # Measurer col-span
+        # Definir o conteúdo
+
+        # ["Alguem Avaliador", "Alguem Avaliado", "Nota", "Nota", "Nota",]
+        for measurer in content.keys():
+            for measured in content[measurer].keys():
+                row = [measurer, measured]
+                row.extend(content[measurer][measured])
+                ws.append(row)
+
+        # Definir o tamanho das células
+
+
+
+
         wb.save(filepath)
     
     
