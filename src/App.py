c1a0d110282cb9912d0a8f6aa8446d194145aeb5
rodrigo0345 <rodrigocralha@gmail.com>
2023-08-18 09:32:38 +0100

    Adding marketing files

--- src/App.py
+++ src/App.py
@@ -10,15 +10,18 @@ from Stategies.AvalStrat.StratParser import StratParser
 
 def transform_excel(config_file, input_file, output_filename):
     layout_input = Layout(True, config_file, input_file)
-    
     excel_interpretor = ExcelInterpretor(layout_input)
 
     avaliation_strategy = StratParser(excel_interpretor)
     question_list = avaliation_strategy.parse()
-
     layout_output = avaliation_strategy.convert_to_layout(question_list)
+
     excel_printer = ExcelPrinter(layout_output)
     excel_printer.print(output_filename)
 
 if __name__ == "__main__":
-    transform_excel("./layouts/RH.json", "./exemplos/Avaliacao-Membro-RH.xlsx", "./output/Output-Avaliacao-Membro-RH.xlsx")
+    transform_excel(
+        config_file="./layouts/RH.json", 
+        input_file="./exemplos/Avaliacao-Membro-RH.xlsx", 
+        output_filename="./output/Output-Avaliacao-Membro-RH.xlsx"
+    )
