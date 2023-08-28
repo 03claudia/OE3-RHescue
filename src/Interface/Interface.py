34f8e2954ca7176f48181d5e48ae0bad49cf520b
Filipe2602 <filipetavares2003@gmail.com>
2023-08-28 17:17:32 +0100

    save

--- src/Interface/Interface.py
+++ src/Interface/Interface.py
@@ -1,38 +1,30 @@
 import streamlit as st
 
-
-
 def interface():
     
-    st.sidebar.title('Escola a opção:')
+    st.title("Avaliação de Desempenho") 
 
-    st.sidebar.button('Layout Marketing')
-    if st.sidebar.button('Layout Recursos Humanos'):
-        st.write('Teste')
+    st.write("\n\n")
 
-    if st.sidebar.button('Exceis de Input'):
+    st.write("Faça o upload dos arquivos para a avaliação de desempenho.")
 
-        st.markdown("<center><h1>Avaliação de Desempenho</h1></center>", unsafe_allow_html=True)
+    files_list=[]
 
-        st.text_input('Escreva algo',value='Teste')
+    num_files = st.slider("Quantidade de Arquivos", min_value=1, max_value=10, value=1)
 
-        st.write("\n\n")
+    for i in range (num_files):
+        file = st.file_uploader(f"Upload do Arquivo {i+1}", type=["xlsx"])
+        if file is not None:
+            files_list.append(file)
 
-        files_list=[]
-
-        num_files = st.slider("Quantidade de Arquivos", min_value=1, max_value=10, value=1)
-
-        for i in range (num_files):
-            file = st.file_uploader(f"Upload do Arquivo {i+1}", type=["xlsx"])
-            if file is not None:
-                files_list.append(file)
-
-        
-        if st.button("Processar Arquivos"):
-            st.write('Ficheiros processados:')
-            for file in files_list:
-                st.write(file.name)
-            return files_list
+    
+    if st.button("Processar Arquivos"):
+       st.write('Ficheiros processados:')
+       for file in files_list:
+           st.write(file.name)
+       return files_list
+    
+  
         
 
 
