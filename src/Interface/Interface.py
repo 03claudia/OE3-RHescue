cd21147e677f97b16b800b3d2f33d1680de06f4c
Filipe2602 <filipetavares2003@gmail.com>
2023-10-15 02:26:44 +0100

    adicionar pergunta ao layout

--- src/Interface/Interface.py
+++ src/Interface/Interface.py
@@ -1,41 +1,105 @@
 import streamlit as st
 import json
 
-options=['Excel de Input','Marketing','Recursos Humanos','VPE']
+Av_Mensal=['Excel de Input','Avaliacao-Membros-MKT','Avaliacao-Membro-RH']
+Av_Trimestral=[]
+Av_Projeto=[]
+Av_Direcao=['Excel de Input','Avaliacao-Vice-Externo']
 
 
 data_mk=None
 
+def abrir(file):
+    file_data= open(f'./layouts/{file}.json', 'r', encoding="UTF-8")
+    data=json.loads(file_data.read())
+
+    return data 
+
+
+def escrever(file,data):
+    with open(f'./layouts/{file}.json', 'w', encoding="UTF-8") as file:
+        file.write(json.dumps(data, indent=2))
+        file.close()
+
+def input(num_files,name,files_list):
+    for i in range (num_files):
+        file = st.file_uploader(f"Upload do Arquivo {name[i+1]}", type=["xlsx"])
+        if file is not None:
+            files_list.append(file)
+    return files_list        
+
+
+def layout(file_name):
+    st.write("\n\n")
+    st.title(f'{file_name}')
+
+    data=abrir(file_name)
+    
+    if st.button('Adicionar uma pergunta'):
+        for i in data['layout']:
+            if 'QUESTIONS'==i['type']:
+                i['questions'].append({'label':'','type':'NUMBER'})
+        escrever(file_name,data)
+
+    for i in data['layout']:
+        if 'MEASURED'== i['type']:
+            names=(i['names'])
+            nome_avaliados=st.text_input('Nomes dos avaliados:',value=names)
+            i['names']=nome_avaliados
+
+    
+    st.write("\n\n")
+
+    for i in data['layout']:
+        if 'MEASURER'== i['type']:
+            name=(i['label'])
+            nome_avaliador=st.text_input('Nome da coluna do avaliador:',value=name)
+            i['label']=nome_avaliador
+
+
+    st.write("\n\n")
+    questions=[]
+    num_question=1
+    for i in data['layout']:
+        if 'QUESTIONS'== i['type']:
+            questoes=(i['questions'])
+            for j in questoes:
+                question=st.text_input(f'Questao {num_question}:',value=j['label'])
+                if j['type']=='NUMBER':
+                    tipo=st.selectbox(f'Escolha a opção da pergunta {num_question}:',['NUMBER','OBSERVATION'])
+                elif j['type']=='OBSERVATION':
+                    tipo=st.selectbox(f'Escolha a opção {num_question}',['OBSERVATION','NUMBER'])
+                
+                if question != '' and tipo != '':
+                    questions.append({'label':question,'type':tipo})
+                num_question+=1
+            i['questions']=questions   
+              
+
+    if st.button('Concluido'):
+        st.write('Alterações efetuadas')
+        escrever(file_name,data)
+        
+
+
+
 
 def interface():
-
-    global data_mk
-    global data_rh
-
-    file_mk= open("./layouts/Avaliacao-Membros-MKT.json", 'r', encoding="UTF-8")
-    data_mk=json.loads(file_mk.read()) 
-
-    file_rh= open("./layouts/Avaliacao-Membro-RH.json", 'r', encoding="UTF-8")
-    data_rh=json.loads(file_rh.read()) 
-
-
-
     ficheiros=None
-    
-    
     st.title("Avaliação de Desempenho") 
    
-        
-    selected_page=st.sidebar.selectbox('Escolha um página',options)
+    st.sidebar.title('Avaliação de Desempenho Mensal')
+    selected_page_Av=st.sidebar.selectbox('Escolha um página',Av_Mensal)
 
-    if selected_page=='Excel de Input':
+    if selected_page_Av=='Excel de Input':
         ficheiros=input()
         
-    if selected_page=='Marketing':
-        data_mk=layout_MK(data_mk)
+    if selected_page_Av=='Avaliacao-Membros-MKT':
+        layout('Avaliacao-Membros-MKT')
         
-    if selected_page=='Recursos Humanos':
-        data_rh=layout_RH(data_rh)      
+        
+    if selected_page_Av=='Recursos Humanos':
+        layout('Avaliacao-Membro-RH')      
 
     if ficheiros is not None: 
         return ficheiros  
@@ -44,21 +108,15 @@ def interface():
    
         
 def input():
+    files_list=[]
     st.write("\n\n")
 
-    st.write("Faça o upload dos arquivos para a avaliação de desempenho.")
+    st.write("Avaliação de Desempenho Mensal.")
+    Num_Av_Mensal=len(Av_Mensal)-1
+    files_list=input(Num_Av_Mensal,Av_Mensal,files_list)
 
-    files_list=[]
+    num_files=Num_Av_Mensal
 
-    num_files = len(options)-1
-    
-
-    for i in range (num_files):
-        file = st.file_uploader(f"Upload do Arquivo {options[i+1]}", type=["xlsx"])
-        if file is not None:
-            files_list.append(file)
-
-    
     if st.button("Processar Arquivos"):
        if len(files_list)==num_files:
             st.write('Ficheiros processados:')
@@ -70,147 +128,3 @@ def input():
 
 
 
-def layout_MK(data):
-
-
-    st.write("\n\n")
-    st.title('Marketing')
-    
-    if st.button('Adicionar uma pergunta'):
-        for i in data['layout']:
-            if 'QUESTIONS'==i['type']:
-                i['questions'].append({'label':'','type':'NUMBER'})
-        with open("./layouts/Avaliacao-Membros-MKT.json", 'w', encoding="UTF-8") as file:
-            file.write(json.dumps(data_mk, indent=2))
-        file.close()
-
-    for i in data['layout']:
-        if 'MEASURED'== i['type']:
-            names=(i['names'])
-            nome_avaliados=st.text_input('Nomes dos avaliados:',value=names)
-            i['names']=nome_avaliados
-
-    
-    st.write("\n\n")
-
-    for i in data['layout']:
-        if 'MEASURER'== i['type']:
-            name=(i['label'])
-            nome_avaliador=st.text_input('Nome da coluna do avaliador:',value=name)
-            i['label']=nome_avaliador
-
-
-    #PARA REMOVER UMA QUESTÃO É SO DEIXAR O ESPAÇO EM BRANCO.
-
-    st.write("\n\n")
-    questions=[]
-    num_question=1
-    for i in data['layout']:
-        if 'QUESTIONS'== i['type']:
-            questoes=(i['questions'])
-            for j in questoes:
-                question=st.text_input(f'Questao {num_question}:',value=j['label'])
-                if j['type']=='NUMBER':
-                    tipo=st.selectbox(f'Escolha a opção da pergunta {num_question}:',['NUMBER','OBSERVATION'])
-                elif j['type']=='OBSERVATION':
-                    tipo=st.selectbox(f'Escolha a opção {num_question}',['OBSERVATION','NUMBER'])
-                
-                if question != '' and tipo != '':
-                    questions.append({'label':question,'type':tipo})
-                num_question+=1
-
-      
-
-            i['questions']=questions   
-              
-
-
-
-
-    if st.button('Concluido'):
-        st.write(data['layout'])
-        with open("./layouts/Avaliacao-Membros-MKT.json", 'w', encoding="UTF-8") as file:
-            file.write(json.dumps(data_mk, indent=2))
-        file.close()
-        return data
-
-        
-      
-      
-
-      
-
-  
-    
-def layout_RH(data):
-
-    st.write("\n\n")
-    st.title('Recursos Humanos')
-    
-    if st.button('Adicionar uma pergunta'):
-        for i in data['layout']:
-            if 'QUESTIONS'==i['type']:
-                i['questions'].append({'label':'','type':'NUMBER'})
-        with open("./layouts/Avaliacao-Membro-RH.json", 'w', encoding="UTF-8") as file:
-            file.write(json.dumps(data_rh, indent=2))
-        file.close()
-
-    for i in data['layout']:
-        if 'MEASURED'== i['type']:
-            names=(i['names'])
-            nome_avaliados=st.text_input('Nomes dos avaliados:',value=names)
-            i['names']=nome_avaliados
-
-    
-    st.write("\n\n")
-
-    for i in data['layout']:
-        if 'MEASURER'== i['type']:
-            name=(i['label'])
-            nome_avaliador=st.text_input('Nome da coluna do avaliador:',value=name)
-            i['label']=nome_avaliador
-
-
-    #PARA REMOVER UMA QUESTÃO É SO DEIXAR O ESPAÇO EM BRANCO.
-
-    st.write("\n\n")
-    questions=[]
-    num_question=1
-    for i in data['layout']:
-        if 'QUESTIONS'== i['type']:
-            questoes=(i['questions'])
-            for j in questoes:
-                question=st.text_input(f'Questao {num_question}:',value=j['label'])
-                if j['type']=='NUMBER':
-                    tipo=st.selectbox(f'Escolha a opção da pergunta {num_question}:',['NUMBER','OBSERVATION'])
-                elif j['type']=='OBSERVATION':
-                    tipo=st.selectbox(f'Escolha a opção {num_question}',['OBSERVATION','NUMBER'])
-                
-                if question != '' and tipo != '':
-                    questions.append({'label':question,'type':tipo})
-                num_question+=1
-
-      
-
-            i['questions']=questions   
-              
-
-
-
-
-    if st.button('Concluido'):
-        st.write(data['layout'])
-        with open("./layouts/Avaliacao-Membro-RH.json", 'w', encoding="UTF-8") as file:
-            file.write(json.dumps(data_rh, indent=2))
-        file.close()
-        return data
-  
-        
-
-
- 
-
-
-
-
-    
